95ce0d2fa8f20891fd0692e405211274882c60bb
Julyan SLABKO <115033204+jslbk@users.noreply.github.com>
2024-02-12 23:07:20 +0200

    changes in base class

--- src/test/java/demoqa/tests/TestBase.java
+++ src/test/java/demoqa/tests/TestBase.java
@@ -1,24 +1,35 @@
 package demoqa.tests;
 
 import api.AuthorizationApi;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import configurations.ConfigReader;
-import configurations.ProjectConfiguration;
-import configurations.web.WebConfig;
 import helpers.Attachments;
 import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
-
-    private static final WebConfig webConfig = ConfigReader.Instance.read();
     AuthorizationApi authApi = new AuthorizationApi();
 
-    @BeforeEach
-    public void setUp() {
-        ProjectConfiguration projectConfiguration = new ProjectConfiguration(webConfig);
-        projectConfiguration.webConfig();
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.browser = "chrome";
+        Configuration.baseUrl = "https://demoqa.com";
+        RestAssured.baseURI = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
